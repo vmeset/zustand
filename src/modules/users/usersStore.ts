e8bc1f2c917065bd0c4c66f4f5fa23f1fa277466
vmeset <vmeset@gmail.com>
2023-03-03 14:30:26 +0300

    immer for mutable state

--- src/modules/users/usersStore.ts
+++ src/modules/users/usersStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { immer } from 'zustand/middleware/immer'
 
 export interface IUser {
     id: number;
@@ -11,25 +12,28 @@ export interface IUsersStore {
   fetchUsers: () => void
 }
 
-export const useUsersStore = create<IUsersStore>((set) => ({
+export const useUsersStore = create<IUsersStore>()(immer((set) => ({
   users: [
     {id:1, username: 'us'},
     {id:11, username: 'usa'}
   ],
-  addUser: (username: string) => set((state) => (
-    {
-      users: [
-        ...state.users,
-        {
+  addUser: (username: string) => set(state => {
+      // users: [
+      //   ...state.users,
+      //   {
+      //     id: Date.now(),
+      //     username
+      //   }
+      // ]
+      state.users.push({
           id: Date.now(),
           username
-        }
-      ]
+      })
     }
-  )),
+  ),
   fetchUsers: async () => {
     const result = await fetch('https://jsonplaceholder.typicode.com/users')
     const json = await result.json() as IUser[]
     set({users: json})
   }
-}))
+})))
